11aab73e4655502c5725a70e04fffccf1936a7bf
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-18 20:27:30 +0200

    Commentaries added in auth.router.js

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -3,5 +3,6 @@ import { authController } from "../controllers/authController.js";
 
 export const authRouter = Router();
 
+// Routes vers le controller d'authentification
 authRouter.get('auth/register', authController.register);
 authRouter.get('auth/login', authController.login);
