85ee0d9b7ce4487282e482c9c36b9fa721e7f79b
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-18 18:23:45 +0200

    Commentaries added on index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 
+// Création du serveur express
 const app = express();
+
+// Attribution du port du serveur express
 const PORT = process.env.PORT || 3000;
 
+// Test pour savoir si le serveur est bien branché au port
 app.listen(PORT, () => {
     console.log(`The server is currently listening events on http://localhost:${PORT}... ✅`)
 });
