7b9a28da2a4c2b5b6b88f265f4ea1d3a79719e26
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-18 19:51:57 +0200

    Display of the view with layout is OK

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import { xss } from "express-xss-sanitizer";
+import expressLayouts from "express-ejs-layouts";
 
 
 // Création du serveur express
@@ -18,12 +19,23 @@ app.use(express.json());
 // Permet de lire et récupérer les requêtes des formulaires HTML
 app.use(express.urlencoded({ extended: true }));
 
+// Permet d'afficher des views à partir d'un layout sans répétition du code
+// le contenu de la view sera généré en fonction de la view indiquée
+app.use(expressLayouts);
+app.set("layout", "./src/views/layouts/app");
+
 // EJS permet d'afficher les views
 app.set('view engine', 'ejs');
+
+// Indique le chemin des views à afficher
 app.set('views', './src/views');
 
+// Indiquer le chemin du layout à afficher 
+app.set('layout', 'layouts/app');
+
+
 app.get('/', (req, res) => {
-    res.render('pages/home');
+    res.render('pages/home', {title : "Accueil"});
 });
 
 // Test pour savoir si le serveur est bien branché au port
