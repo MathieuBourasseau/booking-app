08b0365accb1bfba309a9222cca328148df092dd
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-19 19:28:59 +0200

    register and login routes changed

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,3 +1,6 @@
+import Joi from "joi";
+import { userSchema } from "../schemas/user.schema.js";
+
 export const authController = {
 
     // Méthode pour afficher le form
@@ -9,6 +12,17 @@ export const authController = {
     // Méthode pour créer un compte
     register (req, res) {
         
+        // On souhaite que l'utilisateur crée un compte pour cela : 
+        // On doit récupérer dans la requête les informations nécessaires : username + password 
+        // Et vérifier que les informations envoyées sont au bon format via le schéma. 
+        const { username, password} = Joi(req.body, userSchema);
+
+        
+       
+        // On doit vérifier si l'ID existe. Si il existe on renvoie un message d'erreur
+        // Si c'est ok on créé le nouvel utilisateur
+        // On renvoie la réponse en json 
+
         res.render('pages/register', {title : "Inscription"});
     },
 
