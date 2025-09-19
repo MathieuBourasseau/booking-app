a2d932c48933a4bb78d7184715d95f57433febc2
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-19 14:33:39 +0200

    Move the migrations directory + create table from user model

--- migrations/01.create-tables.js
+++ migrations/01.create-tables.js
@@ -1,4 +1,4 @@
-import { sequelize } from "../models/sequelize.client.js";
+import { sequelize } from "../app/models/sequelize.client.js";
 
 console.log('Création des tables...🚧')
 await sequelize.sync( { force: true} ); // On crée les tables à partir des models
