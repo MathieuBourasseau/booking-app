d479c52bf4f90f8a0e2b5ede217fcecffee5b83d
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-19 14:24:05 +0200

    create-table script added

---
+++ app/migrations/01.create-table.js
@@ -0,0 +1,7 @@
+import { sequelize } from "../models/sequelize.client.js";
+
+console.log('Création des tables...🚧')
+await sequelize.sync( { force: true} ); // On crée les tables à partir des models
+console.log('Tables créées ✅');
+
+await sequelize.close();
