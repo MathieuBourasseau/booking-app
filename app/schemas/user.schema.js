9e0e168afc76d4560b8fc5bdffa4b01e6dc0d485
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-21 17:25:50 +0200

    booking schema created

--- app/schemas/user.schema.js
+++ app/schemas/user.schema.js
@@ -4,3 +4,6 @@ export const userSchema = Joi.object({
     username: Joi.string().min(3).required(), // Le username devra au minimum faire 3 caractères et de type string.
     password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required() // On définit les mots de passe qui seront acceptés. 
 });
+
+
+
