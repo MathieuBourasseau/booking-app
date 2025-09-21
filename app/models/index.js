8da1c84dc5bf37d56cd29d6e3c88b1f25290bffd
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-21 14:20:22 +0200

    imported property model in models/index.js

--- app/models/index.js
+++ app/models/index.js
@@ -1,4 +1,5 @@
 import { sequelize } from "./sequelize.client.js";
+import { Property } from "./property.model.js";
 import { User } from "./user.model.js";
 
-export { sequelize, User };
+export { sequelize, User, Property };
