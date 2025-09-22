eef950b02f2336ea1f1a74c0b9c01095b089ad57
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-22 12:32:19 +0200

    Created index.js in controllers

---
+++ app/controllers/index.js
@@ -0,0 +1,6 @@
+import { authController } from "./authController.js";
+import { bookingsController } from "./bookingsController.js";
+import { homeController } from "./homeController.js";
+import { propertiesController } from "./propertiesController.js";
+
+export  { authController, bookingsController, homeController, propertiesController };
