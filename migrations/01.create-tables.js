4c437534d0e0aabdc0ff841a7d1d5188b01aea32
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-09-25 18:48:02 +0200

    removed the await sequelize.close from create-table.js

--- migrations/01.create-tables.js
+++ migrations/01.create-tables.js
@@ -3,5 +3,3 @@ import { sequelize } from "../app/models/index.js";
 console.log('Création des tables...🚧')
 await sequelize.sync( { force: true} ); // On crée les tables à partir des models
 console.log('Tables créées ✅');
-
-await sequelize.close();
